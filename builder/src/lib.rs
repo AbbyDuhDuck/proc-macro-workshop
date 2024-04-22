011faad1d50e8362e0da80fce464fe6f1cffbe33
mykkee3 <abbyduck96@gmail.com>
2024-04-22 17:55:35 -0400

    builder/test/03 - Pass

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,11 +5,48 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let builder = syn::Ident::new(&format!("{}Builder", name), name.span());
 
     // Build the output, possibly using quasi-quotation
     let expanded = quote! {
-        // ...
+        impl #name {
+            fn builder() -> #builder {
+                #builder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        pub struct #builder {                
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #builder {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self { 
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
     };
 
     // Hand the output tokens back to the compiler
